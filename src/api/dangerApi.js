63a0dae5885ec9b0bb0031d0034f776bec6ab960
Daniel Trujillo <danielfetrujillo@gmail.com>
2020-06-28 12:32:28 -0500

    created custom hook to fetch data form api

---
+++ src/api/dangerApi.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+import dangerApi from '../api/dangerApi';
+
+export default () => {
+
+  const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const getResource = async (resource) => {
+    try {
+      const response = await dangerApi.get(resource);
+      setIsLoading(false);
+      setResults(response.data);
+    } catch (error) {
+      setErrorMessage('Algo salio mal, intentalo de nuevo');
+      setIsLoading(false);
+    }
+  }
+
+  return [getResource, results, errorMessage, isLoading]
+}
