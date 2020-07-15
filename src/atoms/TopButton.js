f5c84511c5cea8ca2dbac36a6233d557a8d6fdb6
Daniel Trujillo <danielfetrujillo@gmail.com>
2020-07-15 16:39:07 -0500

    added behaviour to button

--- src/atoms/TopButton.js
+++ src/atoms/TopButton.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './TopButton.scss';
 
 const TopButton = () => {
 
+  const [showScroll, setShowScroll] = useState(false);
+
+  const checkScrollTop = () => {
+   if (!showScroll && window.pageYOffset > 400){
+      setShowScroll(true)
+    } else if (showScroll && window.pageYOffset <= 400){
+      setShowScroll(false)
+    }
+  };
+
+  window.addEventListener('scroll', checkScrollTop);
+
   const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  if (showScroll) {
+    return (
+      <div className="TopButton" onClick={scrollToTop}>
+        <i aria-hidden="true" className="chevron circle up big link icon"></i>
+      </div>
+    )
   }
-  return (
-    <div className="TopButton" onClick={() => scrollToTop()}>
-      <i aria-hidden="true" class="chevron circle up big link icon"></i>
-    </div>
-  )
+
+  return null
+
 }
 
 export default TopButton
