5e8c09180e38d029afef08137dc3d8ebe1779360
Daniel Trujillo <danielfetrujillo@gmail.com>
2020-07-16 11:28:45 -0500

    i18next configuration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
 import { Provider as TopicsProvider } from './context/TopicsContext';
+import './i18n';
 
 ReactDOM.render(
   <React.StrictMode>
     <TopicsProvider>
-      <App />
+      <Suspense fallback={null}>
+        <App />
+      </Suspense>
     </TopicsProvider>
   </React.StrictMode>,
   document.getElementById('root')
